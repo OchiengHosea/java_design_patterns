c350b00db89201d098eeaa3ab4e9bc6dc2ae64ae
@Ochieng__ <jrhosea8@gmail.com>
2021-02-24 20:40:58 +0300

    builder pattern

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import com.company.builder_patter.LunchOrder;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        LunchOrder.Builder builder = new LunchOrder.Builder();
+        builder.bread("Wheat").dressing("Mayo").meat("Turkey");
+        builder.condiments("ds");
+        LunchOrder lunchOrder = builder.build();
+        System.out.println(lunchOrder);
     }
 }
